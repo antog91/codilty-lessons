881c81a3ca7ef0b9116b6c8b73674586241daba2
antog91 <AntoG91@live,ie>
2021-08-22 20:26:50 -0600

    Added solution to BinaryGap problem

--- src/BinaryGap.java
+++ src/BinaryGap.java
@@ -1,4 +1,27 @@
 
 public class BinaryGap {
-
+	public static int calculateBinaryGap(int number) {
+		int count = 0;
+		int previousCount = 0;
+		
+		String binaryString = Integer.toBinaryString(number);
+		System.out.println(Integer.toBinaryString(number));
+		
+		for(char binaryChar : binaryString.toCharArray()) {
+			if(binaryChar == '1') {
+				if(previousCount < count) {
+					previousCount = count;
+				}
+				count = 0;
+			} else {
+				count++;
+			}
+		}
+		
+		return previousCount;
+	}
+	
+	public static void main(String [] args) {
+		System.out.println(calculateBinaryGap(3343221));
+	}
 }
